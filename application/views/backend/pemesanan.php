b67676566c43def2535d0d6e5f5d00c11e37e1cf
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2024-06-15 01:05:18 +0800

    feature : fix feature penyaluran

--- application/views/backend/pemesanan.php
+++ application/views/backend/pemesanan.php
@@ -52,9 +52,6 @@
 				<div class="col-md-6">
 					<div class="card">
 						<div class="card-header">
-							<?php if ($this->session->userdata('role') == 'admin') { ?>
-								<button class="btn btn-primary" data-toggle="modal" data-target="#tambahpemesanan">Tambah Pesanan</button>
-							<?php } ?>
 							<h3 class="card-title">Riwayat Pemesanan</h3>
 						</div>
 						<div class="card-body">
